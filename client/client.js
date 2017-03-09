ce5c8631db33c1f42d1f38965c0a47dbbcb61d31
grah3367 <grah3367@chawk.cecil.edu>
2017-03-09 11:40:53 -0500

    added chat, login and user functionality

--- client/client.js
+++ client/client.js
@@ -8,9 +8,24 @@ Template.header.events({
 
         console.log("You clicked me!");
 
-    }
+    },
+    'submit form': function (event) {
+
+        event.preventDefault();
+        //console.log("A message was entered: " + event.target.addMessage.value);
+        Messages.insert({
+            user: Meteor.user().emails[0].address,
+            message: event.target.addMessage.value,
+            when: new Date(),
+        });
+
+        event.target.addMessage.value = ''; // reset the value
+
+    },
 });
 
 Template.main.helpers({
-    message: "I'm a MESSAGE!"
+    messages: function () {
+        return Messages.find().fetch();
+    }
 });
